5d82b160b92585d76dc574ccb37a79166c5185d5
victor <user_acid41@skyairline.com>
2021-06-05 13:26:56 -0400

    Including loggers and explanation.txt

--- app.py
+++ app.py
@@ -5,4 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 # Decorador que modifica el metodo que viene despues
 def funcion_inicio():
+    app.logger.debug('Mensaje desde debug')
+    app.logger.info('Mensaje desde debug')
+    app.logger.warning('Mensaje desde warning')
+    app.logger.error('Mensaje desde error')
     return f'Hola Mundo desde Flask'

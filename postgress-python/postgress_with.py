2349d7a41d3acddcacfae032194aa1b37364f2ff
victor <user_acid41@skyairline.com>
2021-06-06 01:38:39 -0400

    Delete one

--- postgress-python/postgress_with.py
+++ postgress-python/postgress_with.py
@@ -2,7 +2,7 @@ import psycopg2
 
 conexion = psycopg2.connect(user = 'postgres', password = 'admin', host= '127.0.0.1', port = '5432', database = 'test_db')
 
-tipo = input("Ingresa lo que harás ['Mostrar todos', 'Mostrar uno', 'Mostrar uno por uno','Insertar uno', 'Insertar varios', 'Actualizar uno', 'Actualizar varios']: ")
+tipo = input("Ingresa lo que harás ['Mostrar todos', 'Mostrar uno', 'Mostrar uno por uno','Insertar uno', 'Insertar varios', 'Actualizar uno', 'Actualizar varios', 'Eliminar uno']: ")
 if tipo == 'Mostrar todos':
     try:
         with conexion:
@@ -121,6 +121,23 @@ if tipo == 'Actualizar varios':
                 cursor.executemany(sentencia, valores)
                 registros_actualizados = cursor.rowcount
                 print(f'Registros actualizados: {registros_actualizados}')
+
+    except Exception as e:
+        print(f'Ocurrio un error: {e}')
+
+    finally:
+        conexion.close()
+
+if tipo == 'Eliminar uno':
+    try:
+        with conexion:
+            with conexion.cursor() as cursor:
+                sentencia = 'DELETE FROM persona WHERE id_persona=%s'
+                valores = (7,)
+
+                cursor.execute(sentencia, valores)
+                registros_eliminados = cursor.rowcount
+                print(f'Registros actualizados: {registros_eliminados}')
 
     except Exception as e:
         print(f'Ocurrio un error: {e}')

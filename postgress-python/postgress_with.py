decc58063bcf1a9f7b17347d4414bcd9a29efbfc
victor <user_acid41@skyairline.com>
2021-06-06 00:58:08 -0400

    Fetchall by input

--- postgress-python/postgress_with.py
+++ postgress-python/postgress_with.py
@@ -2,7 +2,7 @@ import psycopg2
 
 conexion = psycopg2.connect(user = 'postgres', password = 'admin', host= '127.0.0.1', port = '5432', database = 'test_db')
 
-tipo = input("Ingresa lo que harás ['Mostrar todos', 'Mostrar uno']: ")
+tipo = input("Ingresa lo que harás ['Mostrar todos', 'Mostrar uno', 'Mostrar uno por uno']: ")
 if tipo == 'Mostrar todos':
     try:
         with conexion:
@@ -27,6 +27,24 @@ if tipo == 'Mostrar uno':
                 cursor.execute(sentencia, (id_persona,))
                 registros = cursor.fetchone()
                 print(registros)
+
+    except Exception as e:
+        print(f'Ocurrio un error: {e}')
+
+    finally:
+        conexion.close()
+
+if tipo == 'Mostrar uno por uno':
+    try:
+        with conexion:
+            with conexion.cursor() as cursor:
+                sentencia = 'SELECT * FROM persona where id_persona IN %s'
+                entrada = input('Proporciona los id\'s a buscar separados por coma: ')
+                llaves_primarias = (tuple(entrada.split(',')), )
+                cursor.execute(sentencia, llaves_primarias)
+                registros = cursor.fetchall()
+                for registro in registros:
+                    print(registro)
 
     except Exception as e:
         print(f'Ocurrio un error: {e}')
